7253bc5231ed76a553b544dcd4320456d509e391
KOHAT <1220181099@qq.com>
2016-08-02 18:43:53 +0800

    update

--- KOHAT Scan/KOHAT Scan UI.py
+++ KOHAT Scan/KOHAT Scan UI.py
@@ -12,16 +12,14 @@ root.attributes("-topmost", 1)#置顶
 1.基础部件 退出 最大化 最小化 窗口化
 2.基本功能 拉伸放大 子窗口的切换
 3.子窗口内布局 迟点详细画 先弄到基础部件和父窗口布局
-
 参考界面 github
-
 '''
 
-width=1000
+width=1200
 height=600
 w = root.winfo_screenwidth()
 h = root.winfo_screenheight()
-widths=[1000]
+widths=[1200]
 
 class main_windows: #只是为了做区分建了个类 
 
@@ -171,69 +169,277 @@ class main_windows: #只是为了做区分建了个类
 
 
 #功能模块_
+#测试模块界面 基础边框划分
 
-class tester:
+class tester_frame:
 	def Frame(self):
-		Frames=Tkinter.Frame(height=600,width=100)
-		Frames.pack()
+		Frames=Tkinter.Frame(root,height=600,width=100,bg='white')
+		Frames.pack(side='left')
+	def PanedWindow(self):
+		PanedWindow=Tkinter.PanedWindow(orient=VERTICAL)
+		PanedWindow.pack(fill='both',expand=1)
+	def canvas(self):
+		root.geometry(str(widthz)+'x'+str(heightz)+'+10+10')#窗口大小 长*宽
+		canvas = Tkinter.Canvas(root)
+		canvas.configure(width = w)
+		canvas.configure(height = heightz)
+		canvas.configure(bg = "#4a4242")
+		canvas.configure(highlightthickness = 0)
+		canvas.pack()
 
 
-#_________________________________________________做个切换窗口 还是做个 选项
+#测试模块功能按钮 每个功能有自己界面 所以我每个独立出一个类来调用
+color1='#424242' #基本要色
+color2='#715858' #划过时候颜色
+color3='#656565' #按下时候颜色
+widthz=40
+heightz=250
+class KOHAT_spider:
 	def kohat_spider(self):
-		buttonk=Tkinter.Button(root, text='/', command=None,bg='white',relief='flat')
+		global buttonk
+		buttonk=Tkinter.Button(root, text='KOHAT spider', command=None,bg=color1,relief='flat',bd=0,fg='white',activebackground=color3)
+		buttonk.bind('<Enter>',self.color)
+		buttonk.bind('<Leave>',self.color2)
+		buttonk.pack()
+		buttonk.place(bordermode='inside',height=widthz, width=heightz,y=(widthz*2-20))
+
+	def color(self,event):
+		global buttonk
+		buttonk['bg']=color2
+
+	def color2(self,event):
+		global buttonk
+		buttonk['bg']=color1
+
+
+class sql_injection:
 	def sql_injection(self):
-		button_sql_injection=Tkinter.Button(root, text='/', command=None,bg='white',relief='flat')
-		button_sql_injection
+		global button_sql_injection
+		button_sql_injection=Tkinter.Button(root, text='sql injection', command=None,bg=color1,relief='flat',bd=0,fg='white',activebackground=color3)
+		button_sql_injection.bind('<Enter>',self.color)
+		button_sql_injection.bind('<Leave>',self.color2)
+		button_sql_injection.pack()
+		button_sql_injection.place(bordermode='inside',height=widthz, width=heightz,y=(widthz*3-20))
+
+	def color(self,event):
+		global button_sql_injection
+		button_sql_injection['bg']=color2
+
+	def color2(self,event):
+		global button_sql_injection
+		button_sql_injection['bg']=color1
+
+
+class xss_injection:	
 	def xss_injection(self):
-		button_xss_injection=Tkinter.Button(root, text='/', command=None,bg='white',relief='flat')
-		button_xss_injection
+		global button_xss_injection
+		button_xss_injection=Tkinter.Button(root, text='xss injection', command=None,bg=color1,relief='flat',bd=0,activebackground=color3,fg='white')
+		button_xss_injection.bind('<Enter>',self.color)
+		button_xss_injection.bind('<Leave>',self.color2)
+		button_xss_injection.pack()
+		button_xss_injection.place(bordermode='inside',height=widthz, width=heightz,y=(widthz*4-20))
+
+	def color(self,event):
+		global button_xss_injection
+		button_xss_injection['bg']=color2
+
+	def color2(self,event):
+		global button_xss_injection
+		button_xss_injection['bg']=color1
+
+
+class ftp_weak_password:
 	def ftp_weak_password(self):
-		button_ftp_weak_password=Tkinter.Button(root, text='/', command=None,bg='white',relief='flat')
-		button_ftp_weak_password
+		global button_ftp_weak_password
+		button_ftp_weak_password=Tkinter.Button(root, text='ftp weak password', command=None,bg=color1,relief='flat',bd=0,activebackground=color3,fg='white')
+		button_ftp_weak_password.bind('<Enter>',self.color)
+		button_ftp_weak_password.bind('<Leave>',self.color2)
+		button_ftp_weak_password.pack()
+		button_ftp_weak_password.place(bordermode='inside',height=widthz, width=heightz,y=(widthz*5-20))
+
+	def color(self,event):
+		global button_ftp_weak_password
+		button_ftp_weak_password['bg']=color2
+
+	def color2(self,event):
+		global button_ftp_weak_password
+		button_ftp_weak_password['bg']=color1
+
+
+class SSRF:	
 	def SSRF(self):
-		button_SSRF=Tkinter.Button(root, text='/', command=None,bg='white',relief='flat')
-		button_SSRF
+		global button_SSRF
+		button_SSRF=Tkinter.Button(root, text='SSRF', command=None,bg=color1,relief='flat',bd=0,fg='white',activebackground=color3)
+		button_SSRF.bind('<Enter>',self.color)
+		button_SSRF.bind('<Leave>',self.color2)
+		button_SSRF.pack()
+		button_SSRF.place(bordermode='inside',height=widthz, width=heightz,y=(widthz*6-20))
+
+	def color(self,event):
+		global button_SSRF
+		button_SSRF['bg']=color2
+	def color2(self,event):
+		global button_SSRF
+		button_SSRF['bg']=color1
+
+
+class Logical_vulnerability:	
 	def Logical_vulnerability(self):
-		button_Logical_vulnerability=Tkinter.Button(root, text='/', command=None,bg='white',relief='flat')
-		button_Logical_vulnerability
-	def Arbitrary_file_downloa_or_upload(self):
-		button_Arbitrary_file_downloa_or_upload=Tkinter.Button(root, text='/', command=None,bg='white',relief='flat')
-		button_Arbitrary_file_downloa_or_upload
+		global button_Logical_vulnerability
+		button_Logical_vulnerability=Tkinter.Button(root, text='Logical vulnerability', command=None,bg=color1,relief='flat',bd=0,activebackground=color3,fg='white')
+		button_Logical_vulnerability.bind('<Enter>',self.color)
+		button_Logical_vulnerability.bind('<Leave>',self.color2)
+		button_Logical_vulnerability.pack()
+		button_Logical_vulnerability.place(bordermode='inside',height=widthz, width=heightz,y=(widthz*7-20))
+
+	def color(self,event):
+		global button_Logical_vulnerability
+		button_Logical_vulnerability['bg']=color2
+
+	def color2(self,event):
+		global button_xss_injection
+		button_Logical_vulnerability['bg']=color1
+
+class Arbitrary_file_download_or_upload:	
+	def Arbitrary_file_download_or_upload(self):
+		global button_Arbitrary_file_downloa_or_upload
+		button_Arbitrary_file_downloa_or_upload=Tkinter.Button(root, text='Arbitraryd download/upload', command=None,bg=color1,relief='flat',bd=0,activebackground=color3,fg='white')
+		button_Arbitrary_file_downloa_or_upload.bind('<Enter>',self.color)
+		button_Arbitrary_file_downloa_or_upload.bind('<Leave>',self.color2)
+		button_Arbitrary_file_downloa_or_upload.pack()
+		button_Arbitrary_file_downloa_or_upload.place(bordermode='inside',height=widthz, width=heightz,y=(widthz*8-20))
+
+	def color(self,event):
+		global button_Arbitrary_file_downloa_or_upload
+		button_Arbitrary_file_downloa_or_upload['bg']=color2
+
+	def color2(self,event):
+		global button_Arbitrary_file_downloa_or_upload
+		button_Arbitrary_file_downloa_or_upload['bg']=color1
+
+class Xpath_injection:	
 	def Xpath_injection(self):
-		button_Xpath_injection=Tkinter.Button(root, text='/', command=None,bg='white',relief='flat')
-		button_Xpath_injection
+		global button_Xpath_injection
+		button_Xpath_injection=Tkinter.Button(root, text='Xpath injection', command=None,bg=color1,relief='flat',bd=0,activebackground=color3,fg='white')
+		button_Xpath_injection.bind('<Enter>',self.color)
+		button_Xpath_injection.bind('<Leave>',self.color2)
+		button_Xpath_injection.pack()
+		button_Xpath_injection.place(bordermode='inside',height=widthz, width=heightz,y=(widthz*9-20))
+
+	def color(self,event):
+		global button_Xpath_injection
+		button_Xpath_injection['bg']=color2
+
+	def color2(self,event):
+		global button_Xpath_injection
+		button_Xpath_injection['bg']=color1
+
+
+class Xml_injection:	
 	def Xml_injection(self):
-		button_Xml_injection=Tkinter.Button(root, text='/', command=None,bg='white',relief='flat')
-		button_Xml_injection
+		global button_Xml_injection
+		button_Xml_injection=Tkinter.Button(root, text='Xml injection', command=None,bg=color1,relief='flat',bd=0,activebackground=color3,fg='white')
+		button_Xml_injection.bind('<Enter>',self.color)
+		button_Xml_injection.bind('<Leave>',self.color2)
+		button_Xml_injection.pack()
+		button_Xml_injection.place(bordermode='inside',height=widthz, width=heightz,y=(widthz*10-20))
+
+	def color(self,event):
+		global button_Xml_injection
+		button_Xml_injection['bg']=color2
+
+	def color2(self,event):
+		global button_Xml_injection
+		button_Xml_injection['bg']=color1
+
+
+class Json_injection:	
 	def Json_injection(self):
-		button_Json_injection=Tkinter.Button(root, text='/', command=None,bg='white',relief='flat')
-		button_Json_injection
+		global button_Json_injection
+		button_Json_injection=Tkinter.Button(root, text='Json injection', command=None,bg=color1,relief='flat',bd=0,activebackground=color3,fg='white')
+		button_Json_injection.bind('<Enter>',self.color)
+		button_Json_injection.bind('<Leave>',self.color2)
+		button_Json_injection.pack()
+		button_Json_injection.place(bordermode='inside',height=widthz, width=heightz,y=(widthz*11-20))
+
+	def color(self,event):
+		global button_Json_injection
+		button_Json_injection['bg']=color2
+
+	def color2(self,event):
+		global button_Json_injection
+		button_Json_injection['bg']=color1
+
+class HTTP_header_pollute:	
 	def HTTP_header_pollute(self):
-		button_HTTP_header_pollute=Tkinter.Button(root, text='/', command=None,bg='white',relief='flat')
-		button_HTTP_header_pollute
+		global button_HTTP_header_pollute
+		button_HTTP_header_pollute=Tkinter.Button(root, text='HTTP header pollute', command=None,bg=color1,relief='flat',bd=0,activebackground=color3,fg='white')
+		button_HTTP_header_pollute.bind('<Enter>',self.color)
+		button_HTTP_header_pollute.bind('<Leave>',self.color2)
+		button_HTTP_header_pollute.pack()
+		button_HTTP_header_pollute.place(bordermode='inside',height=widthz, width=heightz,y=(widthz*12-20))
+
+	def color(self,event):
+		global button_HTTP_header_pollute
+		button_HTTP_header_pollute['bg']=color2
+
+	def color2(self,event):
+		global button_HTTP_header_pollute
+		button_HTTP_header_pollute['bg']=color1
+
+
+class poc_test:	
 	def poc_test(self):
-		button_poc_test=Tkinter.Button(root, text='/', command=None,bg='white',relief='flat')
-		button_poc_test
+		global button_poc_test
+		button_poc_test=Tkinter.Button(root, text='poc_test', command=None,bg=color1,relief='flat',bd=0,activebackground=color3,fg='white')
+		button_poc_test.bind('<Enter>',self.color)
+		button_poc_test.bind('<Leave>',self.color2)
+		button_poc_test.pack()
+		button_poc_test.place(bordermode='inside',height=widthz, width=heightz,y=(widthz*13-20))
+
+
+	def color(self,event):
+		global button_poc_test
+		button_poc_test['bg']=color2
+
+	def color2(self,event):
+		global button_xss_injection
+		button_poc_test['bg']=color1
+
+
+
+def start_test_button():
+	KOHAT_spider().kohat_spider()
+	sql_injection().sql_injection()
+	xss_injection().xss_injection()
+	ftp_weak_password().ftp_weak_password()
+	SSRF().SSRF()
+	Logical_vulnerability().Logical_vulnerability()
+	Arbitrary_file_download_or_upload().Arbitrary_file_download_or_upload()
+	Xpath_injection().Xpath_injection()
+	Xml_injection().Xml_injection()
+	Json_injection().Json_injection()
+	HTTP_header_pollute().HTTP_header_pollute()
+	poc_test().poc_test()
+
 
 
 if __name__ == '__main__':
 	main_windows().baseUI()
 	main_windows().main_button()
-	tester().Frame()
+	tester_frame().Frame()
+	start_test_button()
+	'''
+	tester_frame().canvas()
+	'''
 	root.mainloop()
 
 '''
 父子窗口的参数传递就类似def 与def 或者 class与class的传递只是要def函数的时候要加入传递功能
-
-
 每次按下一图时,把存放图片的控件烧毁,即存放PHOTOIMAGE的控件烧毁,
-
 然后再打包,然后再放入图片,再设图片定尺寸,图片尺寸的声名要全局性GLOBAL
-
 如果是单改变某控件的组态,这个实例化了控件,其实还是在内存中的,
-
 你把他一拖,啥缩放就没了(也可能是因为不是全局性的变量)
-
 紧记->烧毁->再打包->再自动设定缩放大小(GLOBAL全局变量) 
 '''
 '''
